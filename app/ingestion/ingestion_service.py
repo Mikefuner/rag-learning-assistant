499a822cdea788e59e315b4a8d37b6e84a6b886b
Mykhailo Onufriiev <onufriev.m@gmail.com>
2026-05-06 14:23:59 +0200

    --

--- app/ingestion/ingestion_service.py
+++ app/ingestion/ingestion_service.py
@@ -12,7 +12,7 @@ from typing import List
 load_dotenv()
 db_path = "/home/mikeonuf/PycharmProjects/rag-learning-assistant/db/chroma_db"
 
-splitter = CharacterTextSplitter(chunk_size=200)
+splitter = CharacterTextSplitter(chunk_size=900)
 embedding_function = OpenAIEmbeddings(model="text-embedding-3-small")
 vector_database = Chroma(collection_name="study_material", embedding_function=embedding_function, persist_directory=db_path)
 
@@ -29,10 +29,13 @@ def process_files(files: List[UploadFile]):
         )
 
 def print_chunks(files: List[UploadFile]):
+    chunks_length = []
+
     for file in files:
         text: str = extract_text(file)
         chunks: list[str] = splitter.split_text(text)
-        print(*chunks, sep="\n")
+        chunks_length.append(len(max(chunks, key=len)))
+    print(max(chunks_length))
 
 def delete_collection(collection_name: str):
     vector_database.delete(collection_name=collection_name)
