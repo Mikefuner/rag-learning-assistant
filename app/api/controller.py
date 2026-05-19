af9990debc7e086b3f9e41c6ab31cea1269228f1
Mykhailo Onufriiev <onufriev.m@gmail.com>
2026-05-19 23:12:33 +0200

    Assistant service changed from class to functions format.

--- app/api/controller.py
+++ app/api/controller.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, UploadFile
-from assistant.assistant_service import AssistantService
+from assistant import assistant_service
 from typing import List
 from pydantic import BaseModel
 
@@ -10,16 +10,15 @@ class QueryResponse(BaseModel):
     response: str
 
 router = APIRouter()
-service = AssistantService()
 
 @router.post("/upload_files")
 async def upload_files(files: List[UploadFile]):
-    service.upload_files(files)
+    assistant_service.upload_files(files)
 
 @router.post("/user_query")
 async def user_query(request: QueryRequest):
-    return QueryResponse(response=service.user_query(request.query))
+    return QueryResponse(response=assistant_service.user_query(request.query))
 
 @router.delete("delete_collection/{collection_name}")
 async def delete_collection(collection_name: str):
-    service.delete_collection(collection_name)
+    assistant_service.delete_collection(collection_name)
--- app/assistant/assistant_service.py
+++ app/assistant/assistant_service.py
@@ -2,13 +2,11 @@ from typing import List
 from rag import ingestion_service, generation_service
 from fastapi import UploadFile
 
-class AssistantService:
+def upload_files(self, files: List[UploadFile]):
+    ingestion_service.process_files(files)
 
-    def upload_files(self, files: List[UploadFile]):
-        ingestion_service.process_files(files)
+def delete_collection(self, collection_name: str):
+    return None
 
-    def delete_collection(self, collection_name: str):
-        return None
-
-    def user_query(self, query: str):
-        return generation_service.generate_response(query)
+def user_query(self, query: str):
+    return generation_service.generate_response(query)
