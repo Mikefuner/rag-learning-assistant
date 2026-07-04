d090b6f56e606373528e01124c2e8b4148b3478b
Mykhailo Onufriiev <onufriev.m@gmail.com>
2026-07-05 00:32:35 +0200

    Added a rate limiter for requests in auth.py

--- app/api/controller.py
+++ app/api/controller.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, UploadFile, Depends
 from assistant import assistant_service
-from api.authentication import verify_token
+from security.auth import verify_access
 from typing import List
 from pydantic import BaseModel
 
@@ -11,18 +11,18 @@ class QueryResponse(BaseModel):
     response: str
 router = APIRouter()
 
-@router.post("/upload_files", dependencies=[Depends(verify_token)])
+@router.post("/upload_files", dependencies=[Depends(verify_access)])
 async def upload_files(files: List[UploadFile]):
     assistant_service.upload_files(files)
 
-@router.post("/query_request", dependencies=[Depends(verify_token)])
+@router.post("/query_request", dependencies=[Depends(verify_access)])
 async def query_request(request: QueryRequest):
     return QueryResponse(response=assistant_service.query_request(request.query))
 
-@router.post("/audio_request", dependencies=[Depends(verify_token)])
+@router.post("/audio_request", dependencies=[Depends(verify_access)])
 async def audio_request(audio_file: UploadFile):
     return QueryResponse(response=assistant_service.audio_request(audio_file))
 
-@router.delete("/delete_all", dependencies=[Depends(verify_token)])
+@router.delete("/delete_all", dependencies=[Depends(verify_access)])
 async def delete_all_info():
     assistant_service.delete_all_info()
--- app/security/auth.py
+++ app/security/auth.py
@@ -2,17 +2,25 @@ from dotenv import load_dotenv
 from fastapi import status, HTTPException, Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 import hmac, os
+from security.rate_limiter import RateLimiter
 
 load_dotenv()
 
 API_KEY = os.getenv("API_TOKEN")
 
 security = HTTPBearer()
+rate_limiter = RateLimiter()
 
-async def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+async def verify_access(credentials: HTTPAuthorizationCredentials = Depends(security)):
     token = credentials.credentials
+
     if not hmac.compare_digest(token, API_KEY):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid token"
+        )
+    if not rate_limiter.allow(token):
+        raise HTTPException(
+            status_code=429,
+            detail="Too many requests"
         )
