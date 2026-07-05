ffc8bec6c35f98dfbfa696cc5dba08319e83b065
Mykhailo Onufriiev <onufriev.m@gmail.com>
2026-07-05 23:05:25 +0200

    Rate limiter deleted.

--- app/security/auth.py
+++ app/security/auth.py
@@ -2,14 +2,12 @@ from dotenv import load_dotenv
 from fastapi import status, HTTPException, Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 import hmac, os
-from security.rate_limiter import RateLimiter
 
 load_dotenv()
 
 API_KEY = os.getenv("API_TOKEN")
 
 security = HTTPBearer()
-rate_limiter = RateLimiter()
 
 async def verify_access(credentials: HTTPAuthorizationCredentials = Depends(security)):
     token = credentials.credentials
@@ -18,9 +16,4 @@ async def verify_access(credentials: HTTPAuthorizationCredentials = Depends(secu
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid token"
-        )
-    if not rate_limiter.allow(token):
-        raise HTTPException(
-            status_code=429,
-            detail="Too many requests"
         )
